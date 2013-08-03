64bc2052651283ab23e91952e6b1ba4c3b127ba1
Louis Teboul (Androguide) <louisteboul@gmail.com>
2013-08-03 20:44:18 +0200

    Added Licensing & Copyright

--- APKreator/src/com/androguide/apkreator/helpers/Helpers.java
+++ APKreator/src/com/androguide/apkreator/helpers/Helpers.java
@@ -1,29 +1,32 @@
-/**
- * @author Louis Teboul (Androguide)
+/**   Copyright (C) 2013  Louis Teboul (a.k.a Androguide)
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *    admin@pimpmyrom.org  || louisteboul@gmail.com
+ *    http://pimpmyrom.org || http://androguide.fr
+ *    71 quai Clémenceau, 69300 Caluire-et-Cuire, FRANCE.
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License along
+ *      with this program; if not, write to the Free Software Foundation, Inc.,
+ *      51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ **/
 
 package com.androguide.apkreator.helpers;
 
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.androguide.apkreator.helpers.CMDProcessor.CMDProcessor;
-import com.androguide.apkreator.helpers.CMDProcessor.Constants;
 import com.androguide.apkreator.helpers.CMDProcessor.Shell;
 
 import java.io.BufferedReader;
@@ -43,7 +46,7 @@ import static com.androguide.apkreator.helpers.CMDProcessor.CMDProcessor.runShel
 import static com.androguide.apkreator.helpers.CMDProcessor.CMDProcessor.runSuCommand;
 import static com.androguide.apkreator.helpers.CMDProcessor.CMDProcessor.startSuCommand;
 
-public class Helpers implements Constants {
+public class Helpers {
 
     private static final String BUILD_PROP = "/system/build.prop";
     // don't show unavoidable warnings
@@ -376,7 +379,7 @@ public class Helpers implements Constants {
 
     public static int getNumOfCpus() {
         int numOfCpu = 1;
-        String numOfCpus = Helpers.readOneLine(NUM_OF_CPUS_PATH);
+        String numOfCpus = Helpers.readOneLine("/sys/devices/system/cpu/present");
         String[] cpuCount = numOfCpus.split("-");
         if (cpuCount.length > 1) {
             try {
